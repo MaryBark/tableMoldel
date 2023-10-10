875e231290a00ee3878389adf518cf3f8deebd24
MaryBark <97518012+MaryBark@users.noreply.github.com>
2023-10-10 17:32:49 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
 #include "stateVectorModel.h"
@@ -12,7 +12,9 @@ MainWindow::MainWindow(QWidget *parent)
 
      ui->tabWidget->tabBar()->hide();
 
-     QList <double> mq {10.0 , 10.0 , 10.0 , 10.0 , 10.0 ,10.0};
+//     QList <double> mq {10.0 , 10.0 , 10.0 , 10.0 , 10.0 ,10.0};
+
+     QByteArray mq = QString("123456789345345").toUtf8();
 
     stateVectorModel *statVec = new stateVectorModel(mq, this);
     ui->tableView->setModel(statVec);
@@ -27,11 +29,12 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     ui->tabWidget->setCurrentIndex(0);
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
 
 void MainWindow::on_pushButton_2_clicked()
 {
     ui->tabWidget->setCurrentIndex(1);
+    ui->stackedWidget->setCurrentIndex(1);
 }
-
