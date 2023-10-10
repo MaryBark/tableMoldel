6d58f8680edbc0cacaebb245eb960d8202a20f30
MaryBark <97518012+MaryBark@users.noreply.github.com>
2023-10-10 17:36:10 +0300

    Update stateVectorModel.h

--- stateVectorModel.h
+++ stateVectorModel.h
@@ -15,40 +15,20 @@ class stateVectorModel : public QAbstractTableModel
     Q_OBJECT
 public:
     stateVectorModel(QObject* parent = nullptr);
-    stateVectorModel(const QList <double >, const QObject* parent = nullptr);
-    ~stateVectorModel(){delete m_dataModelptr;}
+//    stateVectorModel(/*const QList <double >*/const QByteArray& dataModel, const QObject* parent = nullptr);
+    stateVectorModel(/*const QList <double >*/const QByteArray& dataModel, const QObject* parent = nullptr);
+    ~stateVectorModel();
 
     // QAbstractItemModel interface
     int rowCount(const QModelIndex &parent) const;
     int columnCount(const QModelIndex &parent) const;
     QVariant data(const QModelIndex &index, int role) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
 
 
 private:
     stateVectorModelPrivate *m_dataModelptr;
 
-
-
 };
-
-
-
-
-
-// Для иконок
-class TableItemDelegate : public QItemDelegate
-{
-    Q_OBJECT
-public:
-    TableItemDelegate();
-
-    QSize sizeHint(const QStyleOptionViewItem &option,
-                       const QModelIndex &index) const override;
-
-    void paint(QPainter *painter, const QStyleOptionViewItem &option,
-                  const QModelIndex &index) const override;
-};
-
-
 
 #endif // STATEVECTORMODEL_H
